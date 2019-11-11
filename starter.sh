33583a643cce345325fbcdda2b390d16c60465b1
herman@unnotech.com <herman@unnotech.com>
2019-11-11 21:20:23 +0800

    changed sh

--- starter.sh
+++ starter.sh
@@ -4,7 +4,7 @@ docker login
 
 # Variable section
 docker_organization=hermandockerhub
-provider_center=female_ghost
+backend=female_ghost
 
 
 git submodule init
@@ -17,29 +17,29 @@ if [ ! -d "female_ghost-postgres" ]; then
   docker-compose up -d female_ghost-postgres
 fi
 
-docker-compose pull $provider_center
-docker-compose up -d $provider_center
+docker-compose pull $backend
+docker-compose up -d $backend
 
 if [ "$initial" == true ]; then
 echo "Waiting for database to load up"
 sleep 90
 echo "Creating Superuser.."
-docker-compose exec $provider_center python manage.py makemigrations
-docker-compose exec $provider_center python manage.py migrate
-docker-compose exec $provider_center python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'pass1234')"  #dev
-cat >> $provider_center-postgres/data/postgresql.conf <<EOF
+docker-compose exec $backend python manage.py makemigrations
+docker-compose exec $backend python manage.py migrate
+docker-compose exec $backend python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'pass1234')"  #dev
+cat >> $backend-postgres/data/postgresql.conf <<EOF
 log_min_duration_statement = 0
 log_line_prefix = '{"Time":[%t], Host:%h} '
 EOF
-docker-compose exec female_ghost-postgres psql -U $provider_center -c "SELECT pg_reload_conf();"
+docker-compose exec female_ghost-postgres psql -U $backend -c "SELECT pg_reload_conf();"
 fi
 
 
-echo "Loading data in $provider_center"
+echo "Loading data in $backend"
 
-docker-compose exec $provider_center sh ./loaddata.sh
-echo "Loading static in $provider_center"
-docker-compose exec $provider_center python manage.py collectstatic --noinput
+docker-compose exec $backend sh ./loaddata.sh
+echo "Loading static in $backend"
+docker-compose exec $backend python manage.py collectstatic --noinput
 
 docker-compose pull #dev
 
@@ -49,7 +49,7 @@ docker-compose up -d  || true  #dev
 
 # Service Access Information Section
 echo "Kindly tests in your local:"
-echo "$provider_center"
+echo "$backend"
 echo "-url: http://localhost:8000/female_ghost_charm/femaleghostcharm/"
 echo "- admin username: admin"
 echo "- admin password: pass1234"
